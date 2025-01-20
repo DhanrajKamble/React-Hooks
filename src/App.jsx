3b2f3ad3551922ffa8abc8e51ac8b68ffb579f35
Dhanraj_Kamble <dhanrajkamble3518@gmail.com>
2025-01-20 13:09:14 +0530

    Dynamic Time Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
 function App(){
+  var time = new Date().toLocaleTimeString();
+
+  const [currentTime, setCurrentTime] = React.useState(time);
+  console.log(time);
+
+  function getTime(){
+  // var time2 = new Date().toLocaleTimeString();
+    setCurrentTime(new Date().toLocaleTimeString());
+  }
+
+  setInterval(getTime,1000);
+
   return (<div>
-    <h1>Practicing React Hooks...!</h1>
+    <h1>{currentTime}</h1>
+    <button onClick = {getTime}>Get Time</button>
   </div>)
 }
 export default App
